5ff9ddf2a76b31040d024bce0e1ee881380ae673
fedyksr <60010253+fedyksr@users.noreply.github.com>
2022-01-25 15:16:12 +0000

    •

---
+++ GestionDeStock/src/main/java/com/fedy/GestionDeStock/model/AbstractEntity.java
@@ -0,0 +1,26 @@
+package com.fedy.GestionDeStock.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Data
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AbstractEntity implements Serializable {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @CreatedDate
+    @Column(name= "creationDate" nullabel = false)
+    private Instant creationDate;
+    @LastModifiedDate
+    @Column(name = "lastModifierDate" nullable = false)
+    private Instant lastModifierDate;
+
+}
